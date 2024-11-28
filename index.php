1e32b731794d6558c72cee1f704cf41ac5a744ef
samuel.lima <samuel.lima@gtplan.com.br>
2024-11-28 16:19:29 -0300

    🚀 [Initial commit]

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Easy Bid</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Cotações Online Rápidas e Práticas</h1>
+        <p>O que deseja comprar:</p>
+        <form action="#">
+            <input type="text" id="search" placeholder="Quero comprar 5 mil reais de coisas incríveis para meu negócio!">
+            <button type="submit">Pesquisar</button>
+        </form>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
